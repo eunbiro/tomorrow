07c99054da175503ca57ce6324f2de7fbdbf0568
github eunbiro <ebee91@naver.com>
2023-02-16 23:55:37 +0900

    0216매장공지폼

--- src/main/java/com/tomorrow/config/SecurityConfig.java
+++ src/main/java/com/tomorrow/config/SecurityConfig.java
@@ -28,16 +28,16 @@ public class SecurityConfig {
 			.loginPage("/member/login")		// 로그인 페이지 url설정
 			.defaultSuccessUrl("/")				// 로그인 성공 시 이동 할 페이지
 			.usernameParameter("userId")			// 로그인 시 사용 할 파라메터 이름
-			.failureUrl("/members/login/error")	// 로그인 실패 시 이동 할 url
+			.failureUrl("/member/login/error")	// 로그인 실패 시 이동 할 url
 			.and()
 			.logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))	// 로그아웃 url
+			.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))	// 로그아웃 url
 			.logoutSuccessUrl("/");				// 로그아웃 성공 시 이동 할 url
 		
 		// 페이지의 접근에 관한 설정
 		http.authorizeRequests()
 			.mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()	// 모든 사용자가 로그인(인증) 없이 접근할 수 있도록 설정
-			.mvcMatchers("/", "/member/**", "/item/**", "/images/**").permitAll()	// 모든 사용자가 로그인(인증) 없이 접근할 수 있도록 설정
+			.mvcMatchers("/", "/member/**", "/item/**", "/images/**", "/shop/**", "/library/**").permitAll()	// 모든 사용자가 로그인(인증) 없이 접근할 수 있도록 설정
 			.mvcMatchers("/admin/**").hasRole("ADMIN")	// '/admin'으로 시작하는 경로페이지는 role이 ADMIN인 사용자만 접근 가능 할 수 있도록 설정
 			.anyRequest().authenticated();				// 그 외의 페이지는 모두 로그인(인증)을 받아야 한다.
 		
---
+++ src/main/java/com/tomorrow/entity/WorkLog.java
@@ -0,0 +1,40 @@
+package com.tomorrow.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "work_log")
+@Getter
+@Setter
+@ToString
+public class WorkLog {
+
+	@Id
+	@Column(name = "log_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;						//  근무일지 식별번호
+	
+	@Column(length = 30, nullable = false)
+	private String partTime;				// 파트타임(오전,오후,야간)
+	
+	@Lob
+	@Column(nullable = false)
+	private String logCont;					// 전달내용
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;					// 회원 식별번호 FK
+}
