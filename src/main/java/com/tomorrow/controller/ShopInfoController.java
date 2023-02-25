9aa0a9b2733e4c75ae94c6cbaa8670f94572c9b2
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-25 17:40:00 +0900

    0225 업데이트 미완

--- src/main/java/com/tomorrow/controller/ShopInfoController.java
+++ src/main/java/com/tomorrow/controller/ShopInfoController.java
@@ -3,11 +3,19 @@ package com.tomorrow.controller;
 import java.security.Principal;
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.tomorrow.dto.CreateShopFormDto;
 import com.tomorrow.dto.MemShopMappingDto;
 import com.tomorrow.dto.MemberFormDto;
 import com.tomorrow.dto.ShopDto;
@@ -22,13 +30,20 @@ import lombok.RequiredArgsConstructor;
 public class ShopInfoController {
 	private final MemberService memberService;
 	private final ShopInfoService shopInfoService;
-
-	/*
-	 * TODO 1. 매장 정보 폼 가져오기 (SELECT) 2. 매장 정보 가져오기 3. 매장 정보 수정하기 4. '취소'버튼 누르면
-	 * 마이페이지로 가게 하기
-	 */
-
-	// 매장정보폼 불러오기 - 수경 1
+	// 원래 마이샵은 상품 등록/수정 페이지랑 상품 상세정보 페이지로 나누어져 있었는데 
+	// 여기는... 상품 등록과 상품 상세정보/수정 페이지로 구분합시다. 
+	// 상품 등록은 정환님이 하니깐... 아 뭔가 마땅한 방법같지 않은데 일단 이렇게 고고
+	// 정환님 페이지 만들고 있는데 거기다가 내가 수정을 끼워넣을 수는 없으니깐 ! ㅎㅎ 
+	
+	/* TODO 수정기능 넣고 난 다음에 할 일 
+	 * 1. 매장 상세정보 페이지 html 수정하기 (input -> div)
+	 * 1-1. 시발 아니면... 상세정보 페이지를... 새로 만들어야 하나...? 개같은 
+	 * 이건 좀 더 생각을 해보자... 하아 근데 매장 상제정보를 input으로 보여줄 수 있냐고...
+	 * 2. 정환님 코드 보고 지도 불러오기 
+	 * 3. 사진 누르면 원본 크기로 사진 창 따로 뜨게 만들기! (도전) 
+	 * */
+	
+	// 매장정보폼 불러오기
 	@GetMapping(value = "/shop/shopInfo")
 	public String shopInfoForAdmin(Model model, Principal principal) {
 		List<MemShopMappingDto> myShopList = shopInfoService.getMyShop(principal.getName());
@@ -54,6 +69,41 @@ public class ShopInfoController {
 
 		return "shop/shopInfo";
 	}
+	
+	/* TODO
+	 * 1. 매장정보 수정 페이지 불러오기
+	 * 2. 매장정보 수정 
+	 * */
+	/*
+	// 매장정보 수정 페이지 보기 
+	@GetMapping(value = "/shop/shopInfo/{shopId}")
+	public String shopDtl(@PathVariable("shopId") Long shopId, Model model, Principal principal) {
+		try {
+			getSideImg(model, principal);
+			CreateShopFormDto createShopFormDto = shopInfoService.getShopInfoDtl(shopId);
+			model.addAttribute(createShopFormDto);
+		} catch(EntityNotFoundException e) {
+			model.addAttribute("createShopFormDto", new CreateShopFormDto());
+			return "shopCreate/shopCreat";
+		}
+		
+		return "shop/shopInfo";
+	}
+	
+	// 매장정보 수정
+	@PostMapping(value = "shop/shopInfo/{shopId}")
+	public String itemUpdate(@Valid CreateShopFormDto createShopFormDto, BindingResult bindingResult, Model model, @RequestParam("shopImgFile") List<MultipartFile> shopImgFileList, Principal principal) {
+		try {
+			getSideImg(model, principal);
+			shopInfoService.updateShopInfo(createShopFormDto, shopImgFileList);
+		} catch (Exception e) {
+			model.addAttribute("errorMessage", "매장 정보 수정 중 에러가 발생하였습니다.");
+		}
+		return "redirect:/";
+	}
+	*/
+	
+	
 
 	// 직원정보 - 수경 2
 	@GetMapping(value = "/shop/employeeInfo")
