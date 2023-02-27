f4185041cbb8dc2a851ef801b5f386c9ac017f1d
peanutBana <diao0321@gmail.com>
2023-02-27 17:55:29 +0900

    0227 managerCommuteForm 추가

--- src/main/java/com/tomorrow/controller/ShopManageController.java
+++ src/main/java/com/tomorrow/controller/ShopManageController.java
@@ -1,14 +1,19 @@
 package com.tomorrow.controller;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tomorrow.dto.CommuteDto;
+import com.tomorrow.dto.MemShopMappingDto;
 import com.tomorrow.dto.MemberFormDto;
+import com.tomorrow.service.CommuteService;
 import com.tomorrow.service.MemberService;
+import com.tomorrow.service.ShopService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping(value = "/admin")
 public class ShopManageController {
+	private final ShopService shopService;
+	private final CommuteService commuteService;
 	private final MemberService memberService;
 
 	// 사이드바 프로필정보 가져옴
@@ -32,5 +39,17 @@ public class ShopManageController {
 		return "shop/employeeInfoForm";
 	}
 
+	// 출퇴근기록 페이지
+		@GetMapping(value = "/commute")
+		public String commute(Model model, Principal principal) {
+
+			List<MemShopMappingDto> myShopList = shopService.getMyShop(principal.getName());
+
+			getSideImg(model, principal);
+			model.addAttribute("myShopList", myShopList);
+			model.addAttribute("commuteDto", new CommuteDto());
+
+			return "manage/managerCommuteForm";
+		}
 
 }
