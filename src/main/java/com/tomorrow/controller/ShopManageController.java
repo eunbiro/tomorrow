53b7ec7f36fa9b55611503e77f73ae09dd901e97
rosencrantz96 <topgdvidsyb@naver.com>
2023-02-28 18:26:47 +0900

    0228 직원정보폼 매장 리스트 불러오기 성공, 직원정보 불러오기는 실패

--- src/main/java/com/tomorrow/controller/ShopManageController.java
+++ src/main/java/com/tomorrow/controller/ShopManageController.java
@@ -13,7 +13,10 @@ import com.tomorrow.dto.CommuteDto;
 import com.tomorrow.dto.MemShopMappingDto;
 import com.tomorrow.dto.MemberFormDto;
 import com.tomorrow.dto.ShopDto;
+import com.tomorrow.entity.MemShopMapping;
+import com.tomorrow.repository.MemShopMapRepository;
 import com.tomorrow.service.CommuteService;
+import com.tomorrow.service.EmployeeInfoService;
 import com.tomorrow.service.MemberService;
 import com.tomorrow.service.ShopService;
 
@@ -26,20 +29,44 @@ public class ShopManageController {
 	private final ShopService shopService;
 	private final CommuteService commuteService;
 	private final MemberService memberService;
-
+	private final EmployeeInfoService emplInfoService;
+	private final MemShopMapRepository mapRepository;
+	
 	// 사이드바 프로필정보 가져옴
 	public Model getSideImg(Model model, Principal principal) {
 		MemberFormDto memberFormDto = memberService.getIdImgUrl(principal.getName());
 		return model.addAttribute("member", memberFormDto);
 	}
-	
 
-	// 직원정보 - 수경 2
+	// 직원정보폼 불러오기
 	@GetMapping(value = "/manage/employeeInfo")
 	public String employeeInfo(Model model, Principal principal) {
 		getSideImg(model, principal);
-		
+
+		// USER_ID로 가지고 있는 매장 리스트 뽑기
+		List<MemShopMappingDto> myShopList = emplInfoService.getMyShopList(principal.getName());
+
+		model.addAttribute("shopDto", new ShopDto()); // 매장 전체 정보를 가지고 있는 DTO
+		model.addAttribute("myShopList", myShopList); // 매장 이름 리스트를 가져옴
+
 		return "manage/employeeInfoForm";
+	}
+
+	// 직원정보 불러오기
+	@GetMapping(value = "/manage/employeeInfo/{shopId}")
+	public String emplInfoDtl(Model model, Principal principal) {
+		getSideImg(model, principal);
+
+		// USER_ID로 가지고 있는 매장 리스트 뽑기
+		List<MemShopMappingDto> myShopList = emplInfoService.getMyShopList(principal.getName());
+		
+		// 하 여기서 막힘... 다시 잘 생각해보자...
+		/* MemShopMapping memShopMapping = mapRepository.findByShopId(); */
+
+		model.addAttribute("shopDto", new ShopDto()); // 매장 전체 정보를 가지고 있는 DTO
+		model.addAttribute("myShopList", myShopList); // 매장 이름 리스트를 가져옴
+
+		return null;
 	}
 
 	// 매니저 출근관리 화면
@@ -54,15 +81,15 @@ public class ShopManageController {
 
 		return "manage/managerCommuteForm";
 	}
-	
-	//매니저 급여관리 화면
+
+	// 매니저 급여관리 화면
 	@GetMapping(value = "/commute/{shopId}")
 	public String getRegister(@PathVariable("shopId") Long shopId, Model model, Principal principal) {
 
 		List<CommuteDto> commuteList = commuteService.getCommuteListForManager(shopId, principal.getName());
-		//매니저 아이디로 매장 목록 띄우기
+		// 매니저 아이디로 매장 목록 띄우기
 		List<MemShopMappingDto> myShopList = shopService.getMyShop(principal.getName());
- 
+
 		CommuteDto commuteDto = new CommuteDto();
 
 		ShopDto shopDto = new ShopDto();
@@ -70,7 +97,7 @@ public class ShopManageController {
 		commuteDto.setShopDto(shopDto);
 
 		getSideImg(model, principal);
-		
+
 		model.addAttribute("myShopList", myShopList);
 		model.addAttribute("commuteList", commuteList);
 		model.addAttribute("commuteDto", commuteDto);
