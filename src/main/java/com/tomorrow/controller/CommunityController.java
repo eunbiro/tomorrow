f457b458b76bba814bee550d3b57abb4eb9bcf52
peanutBana <diao0321@gmail.com>
2023-03-01 14:22:47 +0900

    0301 게시글 삭제 메소드 추가

--- src/main/java/com/tomorrow/controller/CommunityController.java
+++ src/main/java/com/tomorrow/controller/CommunityController.java
@@ -10,14 +10,18 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tomorrow.dto.BoardCommentFormDto;
@@ -165,5 +169,13 @@ public class CommunityController {
 	     return model.addAttribute("member", memberFormDto);
 	}
 	
-	
+	@DeleteMapping(value="/delete/{boardId}")
+	public @ResponseBody ResponseEntity deleteBoard(@PathVariable("boardId") Long boardId, Principal principal) {
+		if(!boardService.validateBoard(boardId, principal.getName())){
+			
+		}
+		
+		return null;
+	}
+
 }
