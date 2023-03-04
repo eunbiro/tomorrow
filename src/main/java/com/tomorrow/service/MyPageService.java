4b732f0acb2dd6ea1e688c53e734d0870d87ec0c
andy981217 <andy981217@gmail.com>
2023-03-04 17:10:34 +0900

    0304 비밀번호 변경 시도 및 내일 다시 진행

---
+++ src/main/java/com/tomorrow/service/MyPageService.java
@@ -0,0 +1,39 @@
+package com.tomorrow.service;
+
+import java.security.Principal;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+
+import com.tomorrow.dto.MemberFormDto;
+import com.tomorrow.entity.Member;
+import com.tomorrow.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MyPageService {
+	
+	private final MemberRepository memberRepository;
+	private final MemberService memberService;
+	
+	public Model getSideImg(Model model, Principal principal) {   
+	     MemberFormDto memberFormDto = memberService.getIdImgUrl(principal.getName());
+	     return model.addAttribute("member", memberFormDto);
+	}
+	
+	public void newPassword(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder, Principal principal, Model model) {
+		
+		//현재 dto에는 로그인 아이디 데이터가 흐르기 떄문에 findByUserId를 쓴다
+		Member member = memberRepository.findByUserId(memberFormDto.getUserId());
+		
+		//dto에 적용시킬 건 password 뿐이기 때문에 memberFormDto.getPassword()로 쓴다
+		member.updatePassword(memberFormDto.getPassword(), passwordEncoder);
+		getSideImg(model, principal);
+	}
+	
+}
