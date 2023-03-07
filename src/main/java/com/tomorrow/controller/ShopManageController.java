6a3dbbd67f33aa1b0cc5581ca5ea79025f47e4d6
rosencrantz96 <topgdvidsyb@naver.com>
2023-03-07 12:28:34 +0900

    0307 수정 중

--- src/main/java/com/tomorrow/controller/ShopManageController.java
+++ src/main/java/com/tomorrow/controller/ShopManageController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -183,5 +185,11 @@ public class ShopManageController {
 	}
 	
 	// 직원 정보 삭제
+	@DeleteMapping(value = "/admin/manage/employeeInfo/{mapId}/delete")
+	public @ResponseBody ResponseEntity deleteEmployee(@PathVariable("mapId") Long mapId, Principal principal) {
+		MemShopMapping memShopMapping = emplInfoService.findMapping(mapId);
+		emplInfoService.deleteEmployee(memShopMapping);
+		return new ResponseEntity<Long>(mapId, HttpStatus.OK);
+	}
 
 }
